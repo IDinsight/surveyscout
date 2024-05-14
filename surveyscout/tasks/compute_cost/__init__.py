f2ac4315dea48d0d6591b4b783d97a2e07586992
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-05-14 13:58:17 +0530

    add google maps cost

--- surveyscout/tasks/compute_cost/__init__.py
+++ surveyscout/tasks/compute_cost/__init__.py
@@ -1,4 +1,11 @@
 from surveyscout.tasks.compute_cost.haversine import get_enum_target_haversine_matrix
 from surveyscout.tasks.compute_cost.osrm import get_enum_target_osrm_matrix
+from surveyscout.tasks.compute_cost.google_distance_matrix import (
+    get_enum_target_google_distance_matrix,
+)
 
-__all__ = ["get_enum_target_haversine_matrix", "get_enum_target_osrm_matrix"]
+__all__ = [
+    "get_enum_target_haversine_matrix",
+    "get_enum_target_osrm_matrix",
+    "get_enum_target_google_distance_matrix",
+]
