4bc7d99105aa63a4fa29a950f1d25d3e5807b693
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-05-29 12:33:42 +0530

    display costs -- only accept nonnull assignment matrix and require cost matrix in postprocessing

--- surveyscout/tasks/postprocessing.py
+++ surveyscout/tasks/postprocessing.py
@@ -6,11 +6,10 @@ from surveyscout.utils import LocationDataset
 
 
 def postprocess_results(
-    results: NDArray | List[List] | None,
+    assignment_matrix: NDArray | List[List],
     enum_locations: LocationDataset,
     target_locations: LocationDataset,
-    enum_target_cost_matrix: NDArray | None = None,
-    **kwargs,
+    enum_target_cost_matrix: pd.DataFrame,
 ) -> pd.DataFrame:
     """
     Processes the raw results from the optimization model, converting them into a
@@ -22,7 +21,7 @@ def postprocess_results(
 
     Parameters
     ----------
-    results : numpy.ndarray or list of lists
+    assignment_matrix : numpy.ndarray or list of lists
         The raw results matrix from the optimization algorithm, with `1` indicating
         an assigned target and `0` otherwise.
 
@@ -33,8 +32,9 @@ def postprocess_results(
         A <LocationDataset> object containing the id and locations of targets, with a
         similar structure to `enum_locations`.
 
-    enum_target_cost_matrix : NDArray
-        Cost matrix
+    enum_target_cost_matrix : pd.DataFrame, optional
+        distance matrix between enumerators and targets.
+        Columns are enumerator IDs, rows are target IDs.
 
     **kwargs : dict, optional
         Additional keyword arguments that can be used in a custom postprocessing step.
@@ -45,12 +45,13 @@ def postprocess_results(
         A DataFrame where each row represents an assignment with columns corresponding
         to target IDs, enumerator IDs, and assigned values (all 1's).
     """
+    assigned_cost_matrix = assignment_matrix.copy()
 
     if enum_target_cost_matrix is not None:
-        results *= enum_target_cost_matrix
+        assigned_cost_matrix *= enum_target_cost_matrix.values
 
     df_results = pd.DataFrame(
-        results,
+        assigned_cost_matrix,
         index=target_locations.get_ids(),
         columns=enum_locations.get_ids(),
     )
